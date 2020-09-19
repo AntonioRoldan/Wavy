e2c83e63efe7cf02510ac5db5cded85db54b693d
AntonioRoldan <antonioroldan95@outlook.com>
2020-09-19 11:47:34 +0100

    Made consumers asynchronous

--- src/workers/album/album.ts
+++ src/workers/album/album.ts
@@ -10,55 +10,90 @@ import { consumeFromQueue } from '../../services/mq'
 var amqp = require('amqplib/callback_api')
 
 export const runUploadConsumer = async () => {
-    await consumeFromQueue(config.queues.uploadAlbum, async (msg: any, ch: any) => {
-      const parameters = JSON.parse(msg.content)
-      const albumServiceInstance = Container.get(AlbumService)
-      const successMessage = await albumServiceInstance.uploadAlbum(parameters.userId, parameters.albumObject, parameters.trackFiles, parameters.coverFile)
-      console.log('Success Message:', successMessage)
-      ch.ack(msg)
-    })
+  return new Promise( async (resolve, reject) => {
+    try { 
+      await consumeFromQueue(config.queues.uploadAlbum, async (msg: any, ch: any) => {
+        const parameters = JSON.parse(msg.content)
+        const albumServiceInstance = Container.get(AlbumService)
+        const successMessage = await albumServiceInstance.uploadAlbum(parameters.userId, parameters.albumObject, parameters.trackFiles, parameters.coverFile)
+        console.log('Success Message:', successMessage)
+        resolve(successMessage)
+        ch.ack(msg)
+      })
+    } catch (err) {
+      reject(err.message || err.msg)
+    }
+  })
 }
 
 export const runAddNewTracksConsumer = async () => {
-  await consumeFromQueue(config.queues.addNewTracksToAlbum, async (msg: any, ch: any) => {
-    const parameters = JSON.parse(msg.content)
-      console.log('parameters :', parameters)
-      const albumServiceInstance = Container.get(AlbumService)
-      const successMessage = await albumServiceInstance.addTracksToExistingAlbum(parameters.userId, parameters.albumId, parameters.trackObjects, parameters.trackFiles)
-      console.log('Success Message:', successMessage)
-      ch.ack(msg)
+  return new Promise( async (resolve, reject) => {
+    try { 
+      await consumeFromQueue(config.queues.addNewTracksToAlbum, async (msg: any, ch: any) => {
+        const parameters = JSON.parse(msg.content)
+          console.log('parameters :', parameters)
+          const albumServiceInstance = Container.get(AlbumService)
+          const successMessage = await albumServiceInstance.addTracksToExistingAlbum(parameters.userId, parameters.albumId, parameters.trackObjects, parameters.trackFiles)
+          console.log('Success Message:', successMessage)
+          resolve(successMessage)
+          ch.ack(msg)
+      })
+    } catch (err) {
+      reject(err.message || err.msg)
+    }
   })
 } 
 
 export const runEditCoverConsumer = async () => {
-  await consumeFromQueue(config.queues.editAlbumCover, async (msg: any, ch: any) => {
-    const parameters = JSON.parse(msg.content)
-      console.log('parameters :', parameters)
-      const albumServiceInstance = Container.get(AlbumService)
-      const successMessage = await albumServiceInstance.editAlbumCover(parameters.userId, parameters.albumId, parameters.coverFile)
-      console.log('Success Message:', successMessage)
-      ch.ack(msg)
+  return new Promise( async (resolve, reject) => {
+    try { 
+      await consumeFromQueue(config.queues.editAlbumCover, async (msg: any, ch: any) => {
+        const parameters = JSON.parse(msg.content)
+        console.log('parameters :', parameters)
+        const albumServiceInstance = Container.get(AlbumService)
+        const successMessage = await albumServiceInstance.editAlbumCover(parameters.userId, parameters.albumId, parameters.coverFile)
+        console.log('Success Message:', successMessage)
+        resolve(successMessage)
+        ch.ack(msg)
+      })
+    } catch (err) {
+      reject(err.message || err.msg)
+    }
   })
 }
 
 export const runDeleteAlbumConsumer = async () => {
-  await consumeFromQueue(config.queues.deleteAlbum, async (msg: any, ch: any) => {
-    const parameters = JSON.parse(msg.content)
-    console.log('parameters :', parameters)
-    const albumServiceInstance = Container.get(AlbumService)
-    const successMessage = await albumServiceInstance.deleteAlbum(parameters.userId, parameters.albumId)
-    console.log('Success Message:', successMessage)
-    ch.ack(msg)  
+  return new Promise( async (resolve, reject) => {
+    try { 
+      await consumeFromQueue(config.queues.deleteAlbum, async (msg: any, ch: any) => {
+        const parameters = JSON.parse(msg.content)
+        console.log('parameters :', parameters)
+        const albumServiceInstance = Container.get(AlbumService)
+        const successMessage = await albumServiceInstance.deleteAlbum(parameters.userId, parameters.albumId)
+        console.log('Success Message:', successMessage)
+        resolve(successMessage)
+        ch.ack(msg)  
+      })
+    } catch (err) {
+      reject(err.message || err.msg)
+    }
   })
 }
 
 export const runDeleteAlbumTrackConsumer = async () => {
-  await consumeFromQueue(config.queues.deleteAlbumTrack, async (msg: any, ch: any) => {
-    const parameters = JSON.parse(msg.content)
-    console.log('parameters :', parameters)
-    const trackServiceInstance = Container.get(TrackService)
-    const successMessage = await trackServiceInstance.deleteTrack(parameters.userId, parameters.trackId)
-    console.log('Success Message:', successMessage)
-    ch.ack(msg)
+  return new Promise( async (resolve, reject) => {
+    try { 
+      await consumeFromQueue(config.queues.deleteAlbumTrack, async (msg: any, ch: any) => {
+        const parameters = JSON.parse(msg.content)
+        console.log('parameters :', parameters)
+        const trackServiceInstance = Container.get(TrackService)
+        const successMessage = await trackServiceInstance.deleteTrack(parameters.userId, parameters.trackId)
+        console.log('Success Message:', successMessage)
+        resolve(successMessage)
+        ch.ack(msg)
+      })
+    } catch (err) {
+      reject(err.message || err.msg)
+    }
   })
 }
